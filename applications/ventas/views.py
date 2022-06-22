b1b99f3696ea9b81808f20cad9cb6e52fae2e779
= <=>
2022-06-21 19:59:06 -0500

    ...

--- applications/ventas/views.py
+++ applications/ventas/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
 
-# Create your views here.
+class Prueba(TemplateView):
+    template_name = 'ventas/prueba.html'
+
+
+class PruebaList(ListView):
+    model = MODEL_NAME
+    template_name: "str"
