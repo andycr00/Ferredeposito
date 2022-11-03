89d34d2e827c335da04584328b3ac20fe6db4d18
= <=>
2022-11-02 21:10:57 -0500

    ...

--- applications/compras/models/proveedores.py
+++ applications/compras/models/proveedores.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Proveedor(models.Model):
     nombre = models.CharField(max_length=128)
-    descripcion = models.CharField(max_length=128)
-    nit = models.CharField(max_length=128)
-    direccion = models.CharField(max_length=128)
-    correo = models.CharField(max_length=128)
-    telefono = models.CharField(max_length=128)
+    descripcion = models.CharField(max_length=128, blank=True, null= True)
+    nit = models.CharField(max_length=128, blank=True, null= True)
+    direccion = models.CharField(max_length=128, blank=True, null= True)
+    correo = models.CharField(max_length=128, blank=True, null= True)
+    telefono = models.CharField(max_length=128, blank=True, null= True)
     creado_en = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     actualizado_en = models.DateTimeField(auto_now=True, null=True, blank=True)
 
